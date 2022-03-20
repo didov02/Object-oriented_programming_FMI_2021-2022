71048f61730c30a077decdc0f1077c837691eba2
Justsvetoslavov <justsvetoslavov@gmail.com>
2022-03-20 12:52:45 +0200

    Update task5.cpp

--- Sem. 02/Practicum/task5.cpp
+++ Sem. 02/Practicum/task5.cpp
@@ -2,92 +2,115 @@
 #include <fstream>
 const int MAX_RELATION_SIZE = 25;
 
-struct Pair {
-    int x;
-    int y;
+struct Pair
+{
+	int x;
+	int y;
 };
-
-struct Relation {
-    Pair pairs[MAX_RELATION_SIZE];
-    int size = 0;
-};
-
-Pair createPair(int a, int b) {
-    return { a, b };
+Pair createPair(int a, int b)
+{
+	return { a, b };
 }
-
 void initPair(Pair& pair, int a, int b) {
-    pair.x = a;
-    pair.y = b;
+	pair.x = a;
+	pair.y = b;
+}
+void writePairToFile(std::ofstream& os, const Pair& pair)
+{
+	os << pair.x << " " << pair.y << std::endl;
+}
+Pair readPairFromFile(std::ifstream& is)
+{
+	int x, y;
+	is >> x >> y;
+	return createPair(x, y);
+}
+void printPair(const Pair& p)
+{
+	std::cout << p.x << " " << p.y << std::endl;
 }
 
-void addPairToRelation(Relation& r, int x, int y) {
-    //
-
-
-    Pair pair = createPair(x, y);
-    r.pairs[r.size++] = pair;
+struct Relation
+{
+	Pair data[MAX_RELATION_SIZE];
+	size_t size = 0;
+};
+bool checkSize(const Relation& r)
+{
+	return r.size < MAX_RELATION_SIZE;
 }
+bool addPairToRelation(Relation& r, int x, int y)
+{
+	if (!checkSize(r))
+		return false;
 
-void writePairToFile(const Pair& pair, std::ofstream& os) {
-    os << pair.x << " " << pair.y << std::endl;
+	Pair pair = createPair(x, y);
+	r.data[r.size++] = pair;
+	return true; //success
 }
+bool addPairToRelation(Relation& r, const Pair& p)
+{
+	if (!checkSize(r))
+		return false;
 
-Pair readPairFromFile(std::ifstream& is) {
-    Pair pair;
-    int x, y;
-    is >> x >> y;
-    initPair(pair, x, y);
-    return pair;
+	r.data[r.size++] = p;
+	return true; //success
 }
+bool writeRelationToFile(const Relation& r, const char* fileName)
+{
+	std::ofstream outFile(fileName);
 
-void writeRelationToFile(const Relation& r, const char* fileName) {
-    std::ofstream outFile(fileName);
+	if (!outFile.is_open()) {
+		std::cout << "Error while opening file";
+		return false;
+	}
 
-    if (!outFile.is_open()) {
-        std::cout << "Error while opening file";
-        return;
-    }
+	outFile << r.size << std::endl;
 
-    outFile << r.size << std::endl;
-
-    for (int i = 0; i < r.size; i++) {
-        writePairToFile(r.pairs[i], outFile);
-    }
+	for (size_t i = 0; i < r.size; i++)
+	{
+		writePairToFile(outFile, r.data[i]);
+	}
+	return true; //success
 }
+Relation readRelationFromFile(const char* fileName)
+{
+	std::ifstream inFile(fileName);
 
-Relation readRelationFromFile(const char* fileName) {
-    std::ifstream inFile(fileName);
-    Relation r;
-    if (!inFile.is_open()) {
-        std::cout << "Error while opening file";
-        return Relation();
-    }
+	Relation R;
+	if (!inFile.is_open()) {
+		std::cout << "Error while opening file";
+		return Relation();
+	}
 
-    int size;
-    inFile >> size;
-    for (int i = 0; i < size; i++) {
-        int x, y;
-        inFile >> x >> y;
-        addPairToRelation(r, x, y);
-    }
+	size_t size;
+	inFile >> size;
+	for (size_t i = 0; i < size; i++)
+	{
+		addPairToRelation(R, readPairFromFile(inFile));
+	}
 
-    return r;
+	inFile.close();
+	return R;
+}
+void printRelation(const Relation& r)
+{
+	for (size_t i = 0; i < r.size; i++)
+		printPair(r.data[i]);
 }
 
 int main() {
-    //A = {1,2,3,4}
-    //Relation r; // <
-    //addPairToRelation(r, 1, 2);
-    //addPairToRelation(r, 1, 3);
-    //addPairToRelation(r, 1, 4);
-    //addPairToRelation(r, 2, 3);
-    //addPairToRelation(r, 2, 4);
-    //addPairToRelation(r, 3, 4);
+	Relation example;
 
-    //writeRelationToFile(r, "relation.txt");
+	addPairToRelation(example, 1, 2);
+	addPairToRelation(example, 1, 3);
+	addPairToRelation(example, 1, 4);
+	addPairToRelation(example, 2, 3);
+	addPairToRelation(example, 2, 4);
+	addPairToRelation(example, 3, 4);
 
-    //Relation r1;
-    //r1 = readRelationFromFile("relation.txt");
-
+	printRelation(example);
+	std::cout << std::endl;
+	writeRelationToFile(example,"relation.txt");
+	printRelation(readRelationFromFile("relation.txt"));
 }
