31309f2ff7dac3484377b19be076888bac8f5d2a
Justsvetoslavov <50053338+Justsvetoslavov@users.noreply.github.com>
2022-03-20 11:22:22 +0200

    Update task3.cpp

--- Sem. 02/Practicum/task3.cpp
+++ Sem. 02/Practicum/task3.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 #include <fstream>
 
+bool checkFile(infstream& file)
+{
+    return file.is_open();
+}
+
 int lengthOfFile(const char* fileName) {
     std::ifstream inFile(fileName);
+    
+    if(!checkFile(inFile))
+        return -1;
+    
     int ctr = 0;
     while (!inFile.eof()) {
         inFile.get();
         ctr++;
     }
 
-    return ctr;
+    return ctr - 1;
 }
 
 int main()
