88fbd5b98a7d93ff4fc63eeb3d0593835d997c47
Nikola Svetoslavov <justsvetoslavov@gmail.com>
2022-04-08 17:18:48 +0300

    Update Test.cpp

--- Sem. 04/IntervalsTask/Test.cpp
+++ Sem. 04/IntervalsTask/Test.cpp
@@ -1,22 +1,15 @@
 #include <iostream>
 #include "Interval.h"
+
 using namespace std;
+
 
 int main()
 {
-    IntervalComponents t1(3, 10);
-    IntervalComponents t2(4, 14); 
-    IntervalComponents t3; // [0, 0]
-    IntervalComponents t4(4, 10);
+	Interval i(1, 11);
 
-   // cout << t1.getLength() << endl;
-   // cout << boolalpha << t1.isInInterval(4);
-   // cout << t2.calcPrimeNumbersInTheInterval();
-   // cout << t4.numberOfPalindroms() << endl;
-   //cout << t2.noRepeatingDigits();
-   //cout << boolalpha << t2.isPowerOfTwo();
+	cout << i.calcDiffrentDigitNumbersInTheInverval() << endl;
 
-   //IntervalComponents t5 = t1.section(t2); 
-   //cout << t5.a << t5.b << endl;
-   // cout << boolalpha << t2.uInterval(t4);
+	cout << i.getLength() << endl;
+	
 }
