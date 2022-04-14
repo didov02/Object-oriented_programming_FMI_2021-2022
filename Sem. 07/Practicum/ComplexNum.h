dc221111afd4c2403d75a0860d45df2e35ff7df3
Nikola Svetoslavov <justsvetoslavov@gmail.com>
2022-04-14 17:49:37 +0300

    remove empty lines

--- Sem. 07/Practicum/ComplexNum.h
+++ Sem. 07/Practicum/ComplexNum.h
@@ -3,15 +3,13 @@
 
 class ComplexNum
 {
-
 private:
 	double real;
 	double imaginary;
-
 public:
-
 	ComplexNum();
 	ComplexNum(double, double);
+	
 	ComplexNum(const ComplexNum&) = default;
 	ComplexNum& operator=(const ComplexNum&) = default;
 	~ComplexNum() = default;
@@ -28,11 +26,10 @@ public:
 	friend ComplexNum operator-(const ComplexNum&, const ComplexNum&);
 	friend ComplexNum operator*(const ComplexNum&, const ComplexNum&);
 	friend ComplexNum operator/(const ComplexNum&, const ComplexNum&);
+	
 	friend ComplexNum operator*(const ComplexNum&, const double);
 	friend ComplexNum operator/(const ComplexNum&, const double);
 
 	friend std::ostream& operator<<(std::ostream& os, const ComplexNum& complexNum);
 	friend std::istream& operator>>(std::istream& is, ComplexNum& complexNum);
-
 };
-
