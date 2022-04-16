4efcf58b800cd1de3483482329766b29d545f490
Nikolay Nikolaev <96292977+SupPanda9@users.noreply.github.com>
2022-04-16 22:34:26 +0300

    Update Cartesian.cpp

--- Sem. 07/Practicum/Nvector/Cartesian.cpp
+++ Sem. 07/Practicum/Nvector/Cartesian.cpp
@@ -7,7 +7,7 @@ Cartesian2D::Cartesian2D(const Nvector& first, const Nvector& second)
 	this->second = second;
 }
 
-Nvector Cartesian2D::getVectorByCoordinates(const Nvector& other)
+Nvector Cartesian2D::getVectorByCoordinates(const Nvector& other) const
 {
 	double coordinates[2];
 	coordinates[0] = other.getCoordinate(0) * first.getCoordinate(0) + other.getCoordinate(1) * second.getCoordinate(0);
@@ -16,7 +16,7 @@ Nvector Cartesian2D::getVectorByCoordinates(const Nvector& other)
 	return temp;
 }
 
-Nvector Cartesian2D::getCoordinatesByVector(const Nvector& other)
+Nvector Cartesian2D::getCoordinatesByVector(const Nvector& other) const
 {
 	double coordinates[2];
 	coordinates[1] = (first.getCoordinate(0) * other.getCoordinate(1) - first.getCoordinate(1) * other.getCoordinate(0)) / (first.getCoordinate(0) * second.getCoordinate(1) - first.getCoordinate(1) * second.getCoordinate(0));
