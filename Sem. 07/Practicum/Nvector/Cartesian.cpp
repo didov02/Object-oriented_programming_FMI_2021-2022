26e3e3b985125d7abeb69204482b446558dd19df
Georgi Terziev <49128895+GeorgiTerziev02@users.noreply.github.com>
2022-04-25 22:00:09 +0300

    Remove deleted method from code

--- Sem. 07/Practicum/Nvector/Cartesian.cpp
+++ Sem. 07/Practicum/Nvector/Cartesian.cpp
@@ -10,8 +10,8 @@ Cartesian2D::Cartesian2D(const Nvector& first, const Nvector& second)
 Nvector Cartesian2D::getVectorByCoordinates(const Nvector& other) const
 {
 	double coordinates[2];
-	coordinates[0] = other.getCoordinate(0) * first.getCoordinate(0) + other.getCoordinate(1) * second.getCoordinate(0);
-	coordinates[1] = other.getCoordinate(0) * first.getCoordinate(1) + other.getCoordinate(1) * second.getCoordinate(1);
+	coordinates[0] = other[0] * first[0] + other[1] * second[0];
+	coordinates[1] = other[0] * first[1] + other[1] * second[1];
 	Nvector temp(coordinates, 2);
 	return temp;
 }
@@ -19,8 +19,8 @@ Nvector Cartesian2D::getVectorByCoordinates(const Nvector& other) const
 Nvector Cartesian2D::getCoordinatesByVector(const Nvector& other) const
 {
 	double coordinates[2];
-	coordinates[1] = (first.getCoordinate(0) * other.getCoordinate(1) - first.getCoordinate(1) * other.getCoordinate(0)) / (first.getCoordinate(0) * second.getCoordinate(1) - first.getCoordinate(1) * second.getCoordinate(0));
-	coordinates[0] = (other.getCoordinate(0) - (second.getCoordinate(0) * coordinates[1]))/first.getCoordinate(0);
+	coordinates[1] = (first[0] * other[1] - first[1] * other[0]) / (first[0] * second[1] - first[1] * second[0]);
+	coordinates[0] = (other[0] - (second[0] * coordinates[1]))/first[0];
 	Nvector temp(coordinates,2);
 	return temp;
 }
