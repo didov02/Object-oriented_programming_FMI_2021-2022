d8a49ef6f05d7489bba370fa83271a1df54ce6d0
Justsvetoslavov <justsvetoslavov@gmail.com>
2022-05-04 00:52:53 +0300

    add Hexviewer

---
+++ Additional/Homeworks/HexViewer/main.cpp
@@ -0,0 +1,24 @@
+/*
+Credit: Angel Dimitriev
+https://github.com/Angeld55
+*/
+
+#include <iostream>
+#include <fstream>
+#include "HexViewer.h"
+using namespace std;
+
+int main()
+{
+	HexViewer h("MyNumber2.dat");
+
+	h.print();
+	h.addByte("FF");
+	h.print();
+
+	h.removeLastByte();
+	h.print();
+
+	h.changeByte(3, "63");
+	h.print();
+}
