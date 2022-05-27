8326fa9eebee233e53a8a97fc5d5002a94891e34
Nikola Svetoslavov <justsvetoslavov@gmail.com>
2022-05-27 23:11:57 +0300

    Add initial commit/Temp solution

---
+++ Additional/Consultations/27.05/2/Shop.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+#include "StoreItem.h"
+
+class Shop
+{
+private:
+	StoreItem** list;
+	size_t capacity, size;
+public:
+	Shop();
+	Shop(const Shop&);
+	Shop& operator=(const Shop&);
+	~Shop();
+
+	bool addItem(const StoreItem*);
+	bool deleteItem(size_t);
+	bool changePrice(size_t, double);
+
+	void print() const;
+private:
+	void copy(const Shop&);
+	void free();
+	void resize();
+};
+
+//Член - данни:
+//o Хетерогенен контейнер за атрикулите в магазина
+// Методи :
+//o Добавяне на продукт(приема продукта като аргумент)
+//o Премахване на продукт(приема индекс)
+//o Промяна на цена на продукт(приема индекс и цена)
+//o Извежда информация за всички продук
---
+++ Additional/Consultations/27.05/2/StoreItem.h
@@ -0,0 +1,35 @@
+﻿#pragma once
+
+class StoreItem
+{
+protected:
+	enum Type
+	{
+		FRUIT,
+		VEGETABLE,
+		UNKNOWN
+	} type;
+private:
+	size_t calories;
+	double price;
+public:
+	StoreItem();
+	StoreItem(size_t, double);
+
+	virtual ~StoreItem() = default;
+
+	void setCalories(size_t);
+	void setPrice(double);
+
+	size_t getCalories() const;
+	double getPrice() const;
+
+	virtual StoreItem* clone() const = 0;
+
+	virtual void print() const;
+};
+
+// Методи :
+//o Get() за всяка член - данна
+//o Set() за всяка член - данна
+//o Конструктор с параметри
